650ca8a5ac03da3e6fea76d4c667b06988c817c0
rimman43 <test@test.com>
2021-08-29 10:36:42 -0400

    update manifest

--- manifests/staging_default.pp
+++ manifests/staging_default.pp
@@ -3,7 +3,7 @@
 # @example
 #   include roles::staging_default
 class puppet_roles::staging_default {
-  include profile::base
-  include profile::apache
-  include profile::mysql::server
+  include puppet_profiles::base
+  include puppet_profiles::apache
+  include puppet_profiles::mysql::server
 }
