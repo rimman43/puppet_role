70004853085132d56b3d2a893621c828360a2bb6
rimman43 <test@test.com>
2021-11-02 15:35:33 -0400

    add

--- manifests/production_default.pp
+++ manifests/production_default.pp
@@ -3,7 +3,7 @@
 # @example
 #   include roles::production_default
 class puppet_roles::production_default {
-#  include puppet_profiles::base
+  include puppet_profiles::base
   include puppet_profiles::apache
 #  include puppet_profiles::mysql::server
 }
