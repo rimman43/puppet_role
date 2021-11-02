4e36c1c9a6864c0051eb4f8b594a6ee7e940d7f8
rimman43 <test@test.com>
2021-11-02 15:39:40 -0400

    add

--- manifests/production_default.pp
+++ manifests/production_default.pp
@@ -2,8 +2,8 @@
 #  Configure a whole Production stack roles
 # @example
 #   include roles::production_default
+#  include puppet_profiles::mysql::server
 class puppet_roles::production_default {
   include puppet_profiles::base
   include puppet_profiles::apache
-#  include puppet_profiles::mysql::server
 }
